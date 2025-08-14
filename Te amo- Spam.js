529bb09f307bfa82a04f39d747471e74a2702359
LoroVvm <Lorenzovieira2@hotmail.com>
2025-08-14 10:51:37 -0300

    Funcional porém o texto não está ideal

--- Te amo- Spam.js
+++ Te amo- Spam.js
@@ -1,29 +1,5 @@
-async function enviarScript(scriptText){
-	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
-	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
-	for(const line of lines){
-		console.log(line)
-	
-		textarea.focus();
-		document.execCommand('insertText', false, line);
-		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
-		setTimeout(() => {
-			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-		}, 100);
-		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
-	}
-	
-	return lines.length;
-}
-
-enviarScript(`
-
+(async () => {
+    const texto = `
 I love you
 Te amo
 Je t’aime
@@ -134,4 +110,25 @@ Eu te amo
 사랑해 (Saranghae)
 أحبك (Uḥibbuka / Uḥibbuki)
 אני אוהב אותך (Ani ohev otach)
-`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
+`;
+
+    const lines = texto.split('\n').map(l => l.trim()).filter(l => l);
+    const main = document.querySelector("#main");
+    const textarea = main.querySelector('[contenteditable="true"]');
+
+    if (!textarea) throw new Error("Não há uma conversa aberta");
+
+    for (const line of lines) {
+        textarea.focus();
+        document.execCommand("insertText", false, line);
+        textarea.dispatchEvent(new InputEvent("input", { bubbles: true }));
+
+        
+        const event = new KeyboardEvent('keydown', { bubbles: true, cancelable: true, key: 'Enter', code: 'Enter' });
+        textarea.dispatchEvent(event);
+
+        await new Promise(r => setTimeout(r, 500)); 
+    }
+
+    console.log(`Código finalizado, ${lines.length} mensagens enviadas`);
+})();
